d433efb73a98696670e930e1e50a9c4872c3efe6
Brian Cooper <brian@adafruit.com>
2020-09-25 19:14:05 -0400

    LeetCode Max Consecutive Ones

---
+++ 2020 Daily Challenges/09-24/maxConsecutiveOnes.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+
+/*
+ * Given a binary array, find the maximum number of consecutive 1 s in this array.
+ *
+ * Example:
+ *   Input: [1, 1, 0, 1, 1, 1]
+ *   Output: 3
+ *
+ * Note:
+ *   The input array will only contain 0 and 1.
+ *   The length of input array is a positive integer and will not exceed 10, 000
+ *
+ * From LeetCode featured set on Arrays: //https:leetcode.com/explore/featured/card/fun-with-arrays/521/introduction/3238/
+ *
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Naive / Brute force O(n) Time, O(1) Space
+var findMaxConsecutiveOnes = function (nums) {
+  let counter = 0;
+  let currMax = 0;
+  nums.forEach(curr => {
+    if (curr === 1) {
+      ++counter;
+    } else {
+      if (counter > currMax) {
+        currMax = counter;
+      }
+      counter = 0;
+    }
+  });
+  if (counter > currMax) {
+    currMax = counter;
+  }
+  return currMax;
+};
+
+findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1]);
